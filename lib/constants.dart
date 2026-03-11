f764e23e2445fafd38571663c4c0548ca063554c
vijaysb0613 <vijaysb2006@gmail.com>
2026-03-11 10:35:21 +0530

    fix/baseurl

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,4 @@
-const String baseUrl = String.fromEnvironment(
-  'BASE_URL',
-  defaultValue: 'http://unifound-backend-vjsb.southeastasia.azurecontainer.io:8080/api',
-);
+const String baseUrl = 'http://unifound-backend-vjsb.southeastasia.azurecontainer.io:8080/api';
 const String loginEndpoint = '/auth/login';
 const String logoutEndpoint = '/auth/logout';
 const String jwtKey = 'jwt_token';
